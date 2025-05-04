524d6f7be4f4b870016283c30be1d39174eb4646
Subsolar <judahchung4@gmail.com>
2025-05-03 18:01:40 -0700

    test

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,7 +1,6 @@
 extends Node2D
 class_name Player
 
-
 @export var moveable: Moveable
 @export var animated_character: AnimatedCharacter
 @export var dust_particle: CPUParticles2D
@@ -21,145 +20,133 @@ var dashes: int = 3
 
 
 func _ready() -> void:
-	# Initialize the player's gun and connect healthbox signals
-	update_gun()
-	Globals.player = self
-	healthbox.take_damage.connect(on_hit)
-	healthbox.died.connect(on_die)
+    update_gun()
+    Globals.player = self
+    healthbox.take_damage.connect(on_hit)
+    healthbox.died.connect(on_die)
 
 
 func _process(delta: float) -> void:
-	# Update gun target and handle player actions
-	gun.target = get_global_mouse_position()
-	move_around(delta)
-	animated_character.animate()
-	animate_modulate(delta)
+    gun.target = get_global_mouse_position()
+    move_around(delta)
+    animated_character.animate()
+    animate_modulate(delta)
 
-	# Reset texture if dashes are available
-	if $animated_character.texture == weak_texture and dashes > 0:
-		$animated_character.texture = normal_texture
+    if $animated_character.texture == weak_texture and dashes > 0:
+        $animated_character.texture = normal_texture
 
-	scroll_guns()
-	dash_action()
-	shoot_action()
+    scroll_guns()
+    dash_action()
+    shoot_action()
 
 
 func move_around(delta: float) -> void:
-	# Handle player movement and particle effects
-	var direction: Vector2 = Input.get_vector("left", "right", "up", "down")
-	if direction != Vector2.ZERO:
-		dust_particle.emitting = true
-		moveable.accel_direction(direction, delta)
-	else:
-		dust_particle.emitting = false
-		moveable.add_friction(delta)
+    var direction: Vector2 = Input.get_vector("left", "right", "up", "down")
+    if direction != Vector2.ZERO:
+        dust_particle.emitting = true
+        moveable.accel_direction(direction, delta)
+    else:
+        dust_particle.emitting = false
+        moveable.add_friction(delta)
 
 
 func shoot_action() -> void:
-	# Handle shooting and dash particle effects
-	if Input.is_action_pressed("shoot"):
-		gun.shoot()
-		moveable.slowdown = gun.slowdown
-	else:
-		moveable.slowdown = 0
+    if Input.is_action_pressed("shoot"):
+        gun.shoot()
+        moveable.slowdown = gun.slowdown
+    else:
+        moveable.slowdown = 0
 
-	if moveable.state == moveable.STATES.dashing:
-		dash_particle.rotation = animated_character.rotation - deg_to_rad(90)
-		dash_particle.emitting = true
+    if moveable.state == moveable.STATES.dashing:
+        dash_particle.rotation = animated_character.rotation - deg_to_rad(90)
+        dash_particle.emitting = true
 
-		if $spawn_dash_ghost.time_left == 0:
-			$spawn_dash_ghost.start()
-			var dash_inst: Node2D = dash_effect_scene.instantiate()
-			dash_inst.rotation = animated_character.rotation
-			dash_inst.position = position
-			dash_inst.scale = animated_character.scale
-			get_parent().add_child(dash_inst)
-	else:
-		dash_particle.rotation = moveable.velocity.angle()
-		dash_particle.emitting = false
+        if $spawn_dash_ghost.time_left == 0:
+            $spawn_dash_ghost.start()
+            var dash_inst: Node2D = dash_effect_scene.instantiate()
+            dash_inst.rotation = animated_character.rotation
+            dash_inst.position = position
+            dash_inst.scale = animated_character.scale
+            get_parent().add_child(dash_inst)
+    else:
+        dash_particle.rotation = moveable.velocity.angle()
+        dash_particle.emitting = false
 
 
 func dash_action() -> void:
-	# Handle dash mechanics and effects
-	if Input.is_action_just_pressed("dash"):
-		if dashes > 0:
-			if $dash_regen.time_left == 0.0:
-				$dash_regen.start()
+    if Input.is_action_just_pressed("dash"):
+        if dashes > 0:
+            if $dash_regen.time_left == 0.0:
+                $dash_regen.start()
 
-			healthbox.invinc = true
-			get_tree().create_timer(0.0006 * moveable.dash_speed).timeout.connect(func():
-				if not invinc:
-					healthbox.invinc = false)
+            healthbox.invinc = true
+            get_tree().create_timer(0.0006 * moveable.dash_speed).timeout.connect(func():
+                if not invinc:
+                    healthbox.invinc = false)
 
-			Globals.camera.screenshake(0.3, 1)
-			$spawn_dash_ghost.start()
-			moveable.dash()
-			dashes -= 1
+            Globals.camera.screenshake(0.3, 1)
+            $spawn_dash_ghost.start()
+            moveable.dash()
+            dashes -= 1
 
-			if dashes == 0:
-				$animated_character.texture = weak_texture
-		else:
-			AudioManager.play_sound(SoundEffect.SOUND_EFFECT_TYPE.DASH_FAIL, position)
+            if dashes == 0:
+                $animated_character.texture = weak_texture
+        else:
+            AudioManager.play_sound(SoundEffect.SOUND_EFFECT_TYPE.DASH_FAIL, position)
 
 
 func animate_modulate(delta: float) -> void:
-	# Animate player transparency for invincibility effect
-	modulate.a = lerp(modulate.a, target_mod, 10 * delta)
-	animated_character.material.set_shader_parameter("alpha", modulate.a)
-	if invinc and abs(modulate.a - target_mod) < 0.05:
-		target_mod = 1.0 if target_mod == 0.0 else 0.0
+    modulate.a = lerp(modulate.a, target_mod, 10 * delta)
+    animated_character.material.set_shader_parameter("alpha", modulate.a)
+    if invinc and abs(modulate.a - target_mod) < 0.05:
+        target_mod = 1.0 if target_mod == 0.0 else 0.0
 
 
 func scroll_guns() -> void:
-	# Switch between guns using input
-	if Input.is_action_just_pressed("scroll up"):
-		AudioManager.play_sound(SoundEffect.SOUND_EFFECT_TYPE.SWITCH_GUN, position)
-		gun_index = (gun_index + 1) % len(guns)
-		update_gun()
+    if Input.is_action_just_pressed("scroll up"):
+        AudioManager.play_sound(SoundEffect.SOUND_EFFECT_TYPE.SWITCH_GUN, position)
+        gun_index = (gun_index + 1) % len(guns)
+        update_gun()
 
-	if Input.is_action_just_pressed("scroll down"):
-		AudioManager.play_sound(SoundEffect.SOUND_EFFECT_TYPE.SWITCH_GUN, position)
-		gun_index = (gun_index - 1) % len(guns)
-		update_gun()
+    if Input.is_action_just_pressed("scroll down"):
+        AudioManager.play_sound(SoundEffect.SOUND_EFFECT_TYPE.SWITCH_GUN, position)
+        gun_index = (gun_index - 1) % len(guns)
+        update_gun()
 
 
 func _on_dash_regen_timeout() -> void:
-	# Regenerate dashes over time
-	dashes += 1
-	if dashes < max_dashes:
-		$dash_regen.start()
+    dashes += 1
+    if dashes < max_dashes:
+        $dash_regen.start()
 
 
 func on_hit() -> void:
-	# Handle player taking damage
-	invinc = true
-	healthbox.invinc = true
-	target_mod = 0.0
+    invinc = true
+    healthbox.invinc = true
+    target_mod = 0.0
 
-	get_tree().create_timer(1.0).timeout.connect(func():
-		healthbox.invinc = false
-		invinc = false
-		target_mod = 1.0)
+    get_tree().create_timer(1.0).timeout.connect(func():
+        healthbox.invinc = false
+        invinc = false
+        target_mod = 1.0)
 
-	Globals.hit_effect(0.1)
+    Globals.hit_effect(0.1)
 
 
 func on_die() -> void:
-	# Handle player death
-	Globals.ui.get_node("anim").play_backwards("forward")
-	invinc = true
-	get_tree().create_timer(0.5).timeout.connect(func():
-		Globals.reset()
-		get_tree().reload_current_scene())
+    Globals.ui.get_node("anim").play_backwards("forward")
+    invinc = true
+    get_tree().create_timer(0.5).timeout.connect(func():
+        Globals.reset()
+        get_tree().reload_current_scene())
 
 
 func update_gun() -> void:
-	# Update the current gun instance
-	if gun:
-		gun.queue_free()
+    if gun:
+        gun.queue_free()
 
-	var inst: Gun = guns[gun_index].instantiate()
-	inst.entity = self
-	inst.look_at(get_global_mouse_position())
-	gun = inst
-	add_child(inst)
+    var inst: Gun = guns[gun_index].instantiate()
+    inst.entity = self
+    inst.look_at(get_global_mouse_position())
+    gun = inst
+    add_child(inst)
